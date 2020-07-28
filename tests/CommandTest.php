e03d7639e5e5b5b2727f05e8a926d680cb24b5f3
Shigeki Tanaka <dev@shigeki.tokyo>
2020-07-29 06:44:55 +0900

    [update] add test.

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -28,4 +28,30 @@ EOF;
 
         $this->assertEquals($expected, (string)$command);
     }
+
+    public function testChangePath()
+    {
+        $command = new Command();
+        $command->domain('domains.example')
+            ->path('/path/to/dig')
+            ->qType('aaaa')
+            ->globalServer('192.168.0.1');
+
+        $expected = <<<EOF
+array (
+  0 => '/path/to/dig',
+  1 => '@192.168.0.1',
+  2 => 'domains.example',
+  3 => 'aaaa',
+  4 => '+noall',
+  5 => '+nocmd',
+  6 => '+ans',
+  7 => '+additional',
+  8 => '+authority',
+  9 => '+timeout=1',
+)
+EOF;
+
+        $this->assertEquals($expected, (string)$command);
+    }
 }
